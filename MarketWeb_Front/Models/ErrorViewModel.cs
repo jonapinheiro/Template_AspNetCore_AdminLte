859ad07901e26139ebcd9d5fcb9dae06ca1982ca
JonathanPinheiro <jonapinheiro@hotmail.com>
2023-05-01 10:47:45 -0300

    Primeiro Commit

---
+++ MarketWeb_Front/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MarketWeb_Front.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
